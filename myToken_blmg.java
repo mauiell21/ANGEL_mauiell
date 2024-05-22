27892dfc2b76aae93f787c3f95c8335664cc7ea3
mauiell21 <422000404@ntc.edu.ph>
2024-05-22 21:15:22 +0800

    myToken_blmg.java

--- myToken_blmg.java
+++ myToken_blmg.java
@@ -1,10 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.12 <0.9.0;
+
 /*
        REQUIREMENTS
     1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
     2. Your contract will have a mapping of addresses to balances (address => uint)
     3. You will have a mint function that takes two parameters: an address and a value. 
        The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
+       of the “sender” address by that amount.
     4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
        It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
        and from the balance of the “sender”.
@@ -13,23 +16,25 @@
 */
 
 contract MyToken {
+    // // @custom:dev-run-script .print/scripts/deploy_my_token.js
+    // Public variables
+    string public tokenName = "Blooming";
+    string public tokenAbbrv = "BLMG";
+    uint public totalSupply = 0;
 
-    // public variables here
-   string public tokenName = "Blooming";
-   string public tokenAbbrv = "BLMG";
-   uint public totalSupply = 0;
-    // mapping variable here
+    // Mapping of addresses to balances
     mapping(address => uint) public balances;
-    // mint function
-   function mint (address _address, uint _value) public {
-       totalSupply += _value;
-       balances[_address] += _value;
-   }
-     // burn function
-   function burn (address _address, uint _value) public {
-       if (balances[_address] >= _value){
-       totalSupply -= _value;
-       balances[_address] -= _value;
-       }
-}
+
+    // Mint function
+    function mint(address _address, uint _value) public {
+        totalSupply += _value;
+        balances[_address] += _value;
+    }
+
+    // Burn function
+    function burn(address _address, uint _value) public {
+        require(balances[_address] >= _value, "Insufficient balance to burn");
+        totalSupply += _value;
+        balances[_address] -= _value;
+    }
 }
